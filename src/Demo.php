ddf428daad8af2d4a0f463e8f3a15ba229c8dd64
Duncan <th710703@gmail.com>
2016-08-18 21:41:30 +0800

    重構 Demo.php

--- src/Demo.php
+++ src/Demo.php
@@ -8,26 +8,28 @@
 include_once __DIR__ . '/../vendor/autoload.php';
 use Mouson\Helpers\SequenceHelper;
 
+/**
+ * @param $helper
+ * @param $value
+ *
+ * @return string
+ */
+function getResult($helper, $value)
+{
+    return sprintf(
+        "%s Sequence %d is %d",
+        $helper->getSequenceName(),
+        $value,
+        $helper->calculate($value)
+    );
+}
+
 $helper = SequenceHelper::make('Fibonacci');
-
 $value = 22;
-$result = sprintf(
-    "%s Sequence %d is %d",
-    $helper->getSequenceName(),
-    $value,
-    $helper->calculate($value)
-);
-
+$result = getResult($helper, $value);
 echo $result . PHP_EOL;
 
 $helper = SequenceHelper::make('Padovan');
-
-$value = 50;
-$result = sprintf(
-    "%s Sequence %d is %d",
-    $helper->getSequenceName(),
-    $value,
-    $helper->calculate($value)
-);
-
+$value  = 50;
+$result = getResult($helper, $value);
 echo $result . PHP_EOL;
