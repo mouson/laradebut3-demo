860c35b2cd085730220d2c4df959c7b20c3b49d9
mouson <mouson@gmail.com>
2016-08-18 21:20:46 +0800

    Issue #3 解決巴多萬數列產生之效能問題
    增加靜態變數儲存

--- src/Helpers/SequenceCalculator/PadovanSequenceCalculator.php
+++ src/Helpers/SequenceCalculator/PadovanSequenceCalculator.php
@@ -5,12 +5,19 @@ namespace Mouson\Helpers\SequenceCalculator;
 class PadovanSequenceCalculator implements CalculatorInterface
 {
 
+    private static $result = array();
+
     public function calculate($value)
     {
+        if (key_exists($value, self::$result)) {
+            return self::$result[$value];
+        }
         if ($value >= 0 && $value <= 2) {
             return 1;
         }
-        return $this->calculate($value-2) + $this->calculate($value-3);
+        self::$result[$value] = $this->calculate($value-2) + $this->calculate($value-3);
+
+        return self::$result[$value];
     }
 
     public function getSequenceName()
