5aabdcbf670db16c4a3e25c2691d1eb111914b8a
Duncan <th710703@gmail.com>
2016-08-18 21:18:27 +0800

    Issue #5 加上巴多萬公式說明

--- src/Helpers/SequenceCalculator/PadovanSequenceCalculator.php
+++ src/Helpers/SequenceCalculator/PadovanSequenceCalculator.php
@@ -7,6 +7,13 @@ class PadovanSequenceCalculator implements CalculatorInterface
 
     public function calculate($value)
     {
+        /**
+         * 公式
+         *
+         * f(0) = f(1) = f(2) = 1
+         * f(n) = f(n-2) + f(n-3)
+         *
+         */
         if ($value >= 0 && $value <= 2) {
             return 1;
         }
